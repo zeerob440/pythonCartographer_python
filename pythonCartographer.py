eb6a3dbaa9e3281604b79fcec2089c838d01b60e
zeerob440 <zeerob440@tutanota.com>
2025-11-15 09:10:23 -0500

    Initial commit

---
+++ pythonCartographer.py
@@ -0,0 +1,324 @@
+print ("This program helps navigate notes and simple structures in the Python language. Zachary Roberts 11 AUG 2.")
+
+# Proudly engineered by Zachary Roberts August, 2025. 
+# "We should now have access to the main facility. Let’s find the map room."-Cortana
+
+# Main menu of program. Prints valid input selection, runs through indefinate loop.     
+def main():# it serves as the main menu of Python Cartographer, one may call it the "index"
+    print ("Basics, variable declarations, data types that sort: 1")
+    print ("Input: 2")
+    print ("IF Else selections, IF IF IF..Else selections IF ELIF ElSE Selections: 3")
+    print ("Input Validation: 4")
+    print ("For Loops: 5")
+    print ("Infinite Loops: 6")
+    print ("Indefinate Loops: 7")
+    print ("Arrays: 8")
+    print ("Populating Arrays: 9")
+    print ("On/Off Switch Loop: 10")
+    select_structure = (input("Input structure you would like to review: "))
+    if select_structure == "1":
+        print ("Accessing Basics...")
+        basics()
+    elif select_structure == "2":
+        print ("Accessing Inputs...")
+        inputs()
+    elif select_structure == "3":
+        print ("Accessing Selections")
+        selection()
+    elif select_structure == "4":
+        print ("Accessing Input Validation...")
+        inputValidation()
+    elif select_structure == "5":
+        print("Accessing For Loops...")
+        for_loops()
+
+def returnsToMAin():# It returns user to main menu at the end of a function. 
+    return_key = (input("Press any key when ready to return to Main Menu. "))
+    while True: 
+        main() 
+
+def basics():# This module provides an overview of the basic structures and data types used in Python.
+    print () 
+    print ("This is and overview of basic declerations of Python structures.")
+    print ()
+    print ("VARIABLES")
+    print ("To declare a variable use singular verbs or nouns in snake_case.")
+    print ('dog = ("Rico") or number = 1')
+    print ()
+    print ("FUNCTIONS")
+    print ("Functions are subroutines with in programs that do something. A single program may have several functions.")
+    print ("Name functions after what part of the program they access in camelCase, OR")
+    print ("Name them a verb for what the function does...")
+    print (" itAddsThings() or main()")
+    print ("To declare a function... def itAddsThings(): or def main(): ")
+    print ("1def itAddsThings\n2  (2+2)")
+    print ("INVOKING FUNCTIONS")
+    print ("Once a function is declared, it needs to be invoked in a subsequent line of the program.")
+    print ("Invoke the function by: itAddsThings()")
+    print ()
+    print ("RULES FOR FUNCTIONS:")
+    print ()
+    print ("Fucntions must be FULLY DECLARED before calling them.\n1 def itAddsThings:()\n2  (2 + 2)\n3itAddsThings()")
+    print ("")
+    print ("DATA TYPES")
+    print ()
+    print ("INTEGER")
+    print ("Integers are numbers 1, 2, -1, 0 -3\nInteger are declared like\nnumber = 4")
+    print ()
+    print ("STRINGS")
+    print ('''Strings are words, or numbers. If a string is a number, it is the name of the number not the value of the number'''
+           )
+    print ('Strings are declared such that:\ndog =("Rico") OR\n my_number = "3"')
+    print ()
+    print ("FLOATS")
+    print ("Floats are decimals such as 7.2, 8.1, -.4")
+    print ("Anytime division is done '/' it returns a float.")
+    print ("Floats are declared the same as integers.")
+    print ("DICTONARIES DICT")
+    print ("Dictionaries consist of two constructs, KEYS and VALUES")
+    print ("KEYS are the UNIQUE IDENTIFIER, or a searchable keyword to access the dictionary.")
+    print ('VALUES are attributes or properties of the key.' \
+    'in other words if it was like an actual dictionary he KEY is the Word, the VALUE is the definition ')
+    print()
+    print("DECLARING A DICTIONARY")
+    print()
+    print("Dictionaries are declared like:")
+    print("marine = {}")
+    print("This is an example of an EMPTY DICTIONARY.")
+    print()
+    print("The following 4 outputs are pulling VALUES from the KEY unsc_marine using .get" \
+    "VALUES of unsc_marine include name, rank, weapon, and ship.")
+    print()
+    #Example of populated dictionary
+    unsc_marine = {
+        "name" : "Johnson",
+        "rank" : "Sergeant",
+        "weapon" : "MA5B",
+        "ship" : "Pillar of Autumn"
+    }
+    print(unsc_marine.get('rank'))
+    print(unsc_marine.get('name'))
+    print(unsc_marine.get('weapon'))
+    print(unsc_marine.get("ship"))
+    print()
+    print("TUPLE")
+    print()
+    print("TUPLE is similar to list except TUPLE is immutable, or it cannot be altered once declared." \
+    "To decalre a TUPLE...")
+    #Example of empty TUPLE
+    print ("snake_stuff = ()")
+    #populate tuple
+    snake_stuff = ("nm7267719","bandana")
+    print (snake_stuff)
+
+    returnsToMAin()  
+
+def inputs(): # This function provides overview of inputs.
+    print ()
+    print ("Inputs are foundational structure in any programming language.\nThey allow the user to provide data into a program.")
+    print ()
+    print ("BUILDING A USER INPUT")
+    print ("To build a user input an INPUT VARIABLE must be declared. Like this:")
+    print ("")
+    print ('1 one_or_two = (input("For selection 1 input 1 for selection 2 input 2: "))\n')
+    print ("")
+    print ("Always leave a null space between the last letter and closing parenthesis to allow user input.")
+    print ()
+    print ("INPUT TRANSLATION")
+    print ()
+    print ("Sometimes a certain data type must be used for an INPUT VARIABLE.\nint is integer\nfloat is a decimal\nstring is string\nbool is boolean")
+    print ("An INPUT VARAIBLE with a forced data type is written as:")
+    print ()
+    print ('1 my_thing = datatype(input("input message here: "))')
+    print ()
+    print ("Therefore a variable needing an integer datatype is:")
+    print ('1 my_number = int(input("pick a number :"))')
+    print ()
+    print ("Example...")
+    print ()
+    print ("This varible has been created a float INPUT VARIABLE.\nmy_number = float(input('Input a number: '")
+    print ()
+    my_number = float(input("Input a number: "))
+    print ("This has prompted the user to input a number, my_number is converted to a float.")
+    print (my_number)
+    print ("As a result it outputs a decimal number.")
+    
+    returnsToMAin()  
+def selection(): # This function highlights selection basics.
+    print ()
+    print ("SELECTIONS")
+    print ()  
+    print ('''Selections are an integral software structure used in almost all programs.\nSelections include:''')
+    print ()
+    print ("IF, ELSE SELECTIONS\nIF, ELIF, ELSE SELECTIONS\nIF,IF,ELSE SELECTIONS\nIF IF IF ... SELECTIONS.")
+    print ()
+    print ("IF, ELSE SELETIONS")
+    print ("IF, ELSE SELECTIONS, create a condition test that executes once branch of the selection if true.")
+    print ("It executes the oppisite branch if the condition is not true.\n IT IS IMPORTANT THAT ELSE DOES NOT REQUIRE A CONDITION.")
+    print ()
+    print ("IF, ELSE SELECTION EXAMPLE")
+    print ()
+    print ('1 branch_a_b = (input("Input a for branch a OR any other key for branch b: "))')
+    print ('''2 if branch_a_b == "a":\n3    print("You have selected branch a.")\n4 else: print ("You have selected branch b.")''')
+    print ()
+    branch_a_b = (input("Input a for branch a OR any other key for branch b: "))
+    # if-else example.
+    if branch_a_b == "a":
+        print ("You have selected branch a.")
+    else: print ("You have selected branch b.")
+    print ()
+    print ("The if else selection will print which selction was chosen by the user.")
+    print ()
+    print ("IF IF IF... ELSE SELECTIONS")
+    print ()
+    print ("IF IF IF ...ELSE SELECTIONS are evaluated as 3 seperate distinct selections by Python.")
+    print ("However, in this structure, THE ELSE STATEMENT DOES NOT NEED A CONDITION, IT TRIGGERS WHEN THE PREVIOUS")
+    print ("IF AND ONLY IF THE PREVIOUS IF CONDITION TEST IS FALSE.")
+    print ('''The IF IF IF ELSE STRUCTURE has a very narrow use case.It is best to think of it as
+           2 seperate if selections with no else statement and one if else selection.''')
+    print ()
+    print ("IF IF IF ELSE SELECTION EXAMPLE")
+    print ()
+    # if if...else example.
+    quantity = int(input("Select a number between 1-3: "))
+    if quantity >= 1:
+        print ("Quantity is greater than or equal to 1.")
+    if quantity >= 2:
+        print ("Quantity is greater than or eqaul to 2.")
+    if quantity >= 3:
+        print ("Three is greater than 1 and 2.")
+    else: print("Quantity is greater than or equal to 3, OR equal to 2, OR less than or equal to 1.")
+    print ()
+    print ("IF IF ... SELECTIONS")
+    print ("In an IF IF Selection... ")
+    print ("Python will search the entire structure for true condition tests and return all true condition tests.")
+    print ("Therefore, it will continue to search the structure even after it has found the first true condition test.")
+    print ()
+    # if if ... example.
+    legion_says = int(input("Enter a number between 1-3: "))
+    if legion_says <= 1:
+        print ("Heretics say 1 is less than 2.")
+    if legion_says <=2:
+        print ("Heretics say 2 is greater than 1.")
+    if legion_says == 3:
+        print ("Geth say 3 is greater than 1 and 2.")
+    if legion_says >3:
+        print("You do not follow directions Shepard Commander, you must be indoctrinated.")
+    print ()
+    print ("In this example, if you picked 1 or 2 the program would have returned:")
+    print ("1 Heritics say 2 is greater than 1\n2 Heretics say 2 is greater than 1")
+    print ('It would have skipped "Geth say 3 is greater than 1 and 2"')
+    print ()
+    print ("If 3 was chosen, it would have printed Legion's line from Mass Effect 2.")
+    print ("If > 3 it would have admonished you for not following directions.")
+    print ()
+    print ("IF ELIF ELSE SELECTIONS")
+    print ("IF ELIF ELSE SELECTIONS are a structure that will ONLY EXECUTE THE FIRST CONDITION THAT IS TRUE.")
+    print ("If greater than 3 conditions are needed to complete this sort of structure, ONLY ADDITIONAL ELIF Conditions are created.")
+    print ()
+    print ("Next we'll explore a IF ELIF ELSE SELECTION using the Sorting Hat from Harry Potter.")
+    print ()
+    # if elif else example
+    sorting_hat_selection = (input("What virtue do you value most? bravery, intelligence, or friendship: "))
+    if sorting_hat_selection == ("bravery"):
+        print ("You are in House Gryffindor")
+    elif sorting_hat_selection == ("intelligence"):
+        print ("You are in House Ravenclaw.")
+    elif sorting_hat_selection == "friendship":
+        print ("You are in House Hufflepuff.")
+    else: print ("Your are in House Slytherin.")
+    print ()
+    print ("Since this is an IF ELIF ELIF ELIF ELSE structure it will only return the true statement.")
+    print ('''IF "bravery was selected, the user is Gryffindor; intelligence Ravenclaw
+           friendship Hufflepuff.And if you don't follow rules, the else statement captures
+           all other values, Slytherin.''')
+    print ()
+    print ("This concludes the selection module. ")
+
+    returnsToMAin()
+
+def inputValidation(): # input validation function.
+    print ('''Input validation is the process of forcing user input to adhear to specific data types. It is similar to the feeding ramp between the magazine 
+           of a firearm and the chamber. Input validation forces intent into the correct postion, thereby allowing the program to execute as designed.''')
+    print ()
+    print ("TRY EXCEPT SELECTIONS ")
+    print ("TRY EXCEPT SELECTIONS are usually contained in a WHILE lOOP. This structure indefinately prompts the user to input info until the correct data type is used.")
+    print ("EXAMPLE")
+    print ()
+    print ("In this example the user is prompted to enter an integer.")
+    print ("The TRY EXCEPT structure is nested within a WHILE LOOP. If the user does not enter a integer, the EXCEPT BRANCH continues the loop.")
+    print ("In order for the EXCEPT BRANCH to work, THE ERROR THAT WILL OCCUR MUST BE DEFINED, such that instead of crashing at ValueError, it simply continues")
+    print ("the loop when ValueError is returned by the EXCEPT BRANCH.")
+    print ("In other words, the EXCEPT BRANCH directs instead of chashing continue the loop.")
+    print ("If the user enters an integer the TRY BRANCH executes, and the loop breaks with the BREAK commannd.")
+    print ()
+    print ("Do not enter an integer first to experiment with the TRY EXCEPT structure.")
+    # Try Except example.
+    while True:
+        try:
+            user_integer = int(input("Enter an integer: "))
+            print(f'You enter entered {user_integer}.')
+            break
+        except ValueError:
+            print ("Invalid input, please enter an integer.")
+    print ()
+    print ("TRY EXCEPT ElSE FINALLY Structures")
+    print ()
+    print ("In these sort of structures:")
+    print ("Try is try this operation that may include an error, such as entering a string into an integer input.")
+    print ("Except must be a defined error such as ValueError, It instructs the program what to do when the error is trigged INSTEAD OF CRASHING.")
+    print ("Else is this case only runs if the try attempt succedes, it is an optional branch.Otherwise, one can just run a print command with the try branch.")
+    print ("Finally run no matter what happens, it is also an optional branch. ")
+    print ()
+    print ("IF EXCEPT ELSE FINALLY Structure Example")
+    print ()
+    # Try Except Else Finally Structure
+    print ("Try this TRY EXCEPT ELSE FINALLY Structure.\nEnter a string first.")
+    while True:
+        try: 
+            integer_two = int(input("Enter another integer: "))
+            print (f'You entered {integer_two}.')
+        except ValueError:
+            print("You did not enter an integer. ")
+        else: 
+            print ("The else branch says, you have entered an integer.ELSE is optional. If structure is nested in WHILE Loop, break loop here.")
+            break
+        finally: print("The finally branch don't care what the user does, it will always print. EVEN IF A LOOP BREAKS BEFOREHAND.")
+    print ()
+    print ("This structure enters a WHILE LOOP until a integer is entered\nif an integer is entered the try, else and finally branches activate.")
+    print ("If a non-integer value is entered the loop will execute the EXCEPT branch and reprompt the user")
+    print ()
+    print ("TRY EXCEPT EXCEPT...STRUCTURE")
+
+def for_loops():
+    print("FOR LOOPS")
+    print()
+    print("FOR LOOPS are a type of indefinate loop. They are used to do various tasks such as populating LISTS, completing a task in a set amount of iterations.")
+    print("FOR LOOPS require a PRIMING VALUE,and a Counter(INCRMENTOR OR DECREMENTOR).")
+    print('FOR LOOPS are often described as "for i in range of.." in class room settings.')
+    #Create simple "for i in range of" FOR LOOP
+    print()
+    print("The following is a hardcoded simple FOR LOOP that prints number of iterations.")
+    print()
+    for i in range (3):
+        print (i)
+    print("In the above example the you will see...")
+    print("0")
+    print("1")
+    print("2")
+    print("That is because all iterations start with 0 by defult in Python. Unless specified otherwise.")
+    print()
+    print("RANGE CONTROL EXAMPLE")
+    print()
+    print('To control range in a for loop, it can be declared as\nfor i in range (1, 4):')
+    for i in range (1,4):
+        print(i)
+    print()
+    print("The RANGE CONTROL in this example starts at 1, and ALWAYS END AT THE HIGHEST DECLARED NUMBER - 1.")
+    
+    returnsToMAin()
+
+main()     
+
+
+
