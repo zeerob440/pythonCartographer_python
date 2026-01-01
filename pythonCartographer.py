fe502796a4781ca9c8745037c6d8707c7736f5f7
zeerob440 <zeerob440@tutanota.com>
2026-01-01 16:11:58 -0500

    feat: added selection() function content and daughter functions: selectionBasics(), ifElseSelections(), ifIfIfSelections(), ifElifElseSelections

--- pythonCartographer.py
+++ pythonCartographer.py
@@ -178,7 +178,7 @@ def basics():
         if basic_selection == 1:
             print(atopic)
             variableDeclaration()
-        elif basic_selection ==2:
+        elif basic_selection == 2:
             print(atopic)
             functionBasics()
         elif basic_selection == 3:
@@ -264,7 +264,7 @@ def inputs():
         if input_selection == 1:
             print(atopic)
             inputBasics()
-        elif input_selection ==2:
+        elif input_selection == 2:
             print(atopic)
             inputTranslation()
         else:
@@ -272,141 +272,185 @@ def inputs():
             break
      
 def selection(): # This function highlights selection basics.
-    print ('''
-        SELECTIONS\n
-        Selections are an integral software structure used in almost all programs.
-        Selections include:
-        IF, ELSE SELECTIONS\n
-        IF, ELIF, ELSE SELECTIONS\n
-        IF,IF,ELSE SELECTIONS\n
-        IF IF IF ... SELECTIONS.\n
+
+    def selectionBasics():
+        print ('''
+            SELECTIONS\n
+            Selections are an integral software structure used in almost all programs.
+            It is one of the methods used to conduct programming logic.\n
+            Selections include:
+            IF, ELSE SELECTIONS\n
+            IF, ELIF, ELSE SELECTIONS\n
+            IF IF IF ... SELECTIONS.\n
         ''')
 
-    proceed()
-
-    print('''
-        IF, ELSE SELECTIONS\n
-        IF, ELSE SELECTIONS, create a condition test that executes once branch of the selection if true.
-        It executes the opposite branch if the condition is not true.
-        IT IS IMPORTANT THAT ELSE DOES NOT REQUIRE A CONDITION\n
-        IF, ELSE SELECTION EXAMPLE\n
-        1 branch_a_b = (input("Input a for branch a OR any other key for branch b: "))
-        2 if branch_a_b == "a":
-        3    print("You have selected branch a.")
-        4 else: print ("You have selected branch b.")\n
-        ''')
-   
-    branch_a_b = (input("Input a for branch a OR any other key for branch b: "))
-    # if-else example.
-    if branch_a_b == "a":
-        print ("You have selected branch a.\n")
-    else: print ("You have selected branch b.\n")
-   
-    print ('The if else selection will print which selection was chosen by the user.\n')
-
-    proceed()
+        proceedToMenu()
     
-    print('''
-        IF IF IF... SELECTIONS\n
-        IF IF IF ...SELECTIONS will search the entire structure and return ALL THE TRUE STATEMENTS.
-        IF IF IF...SELECTIONS are useful for finding things.
-        in the below example we find an IF IF IF... SELECTION\n
+    def ifElseSelections():
+
+        print('''
+            IF, ELSE SELECTIONS\n
+            IF, ELSE SELECTIONS, create a condition test that executes once branch of the selection if true.
+            It executes the opposite branch if the condition is not true.
+            IT IS IMPORTANT THAT ELSE DOES NOT REQUIRE A CONDITION\n
+            IF, ELSE SELECTION EXAMPLE\n
+            1 branch_a_b = (input("Input a for branch a OR any other key for branch b: "))
+            2 if branch_a_b == "a":
+            3    print("You have selected branch a.")
+            4 else: print ("You have selected branch b.")\n
+        ''')
+   
+        branch_a_b = (input("Input a for branch a OR any other key for branch b: "))
+            # if-else example.
+        if branch_a_b == "a":
+                print ("You have selected branch a.\n")
+        else: print ("You have selected branch b.\n")
+   
+        print ('The if else selection will print which selection was chosen by the user.\n')
+
+        proceed()
+    
+    def ifIfIfSelections():
+        print('''
+            IF IF IF... SELECTIONS\n
+            IF IF IF ...SELECTIONS will search the entire structure and return ALL THE TRUE STATEMENTS.
+            IF IF IF...SELECTIONS are useful for finding things.
+            in the below example we find an IF IF IF... SELECTION\n
           
-        quantity = int(input("Select a number between 1-3: "))
+            quantity = int(input("Select a number between 1-3: "))
+            if quantity >= 1:
+                print ("Quantity is greater than or equal to 1. ")
+            if quantity >= 2:
+                print ("Quantity is greater than or equal to 2. ")
+            if quantity >= 3:
+                print ("Quantity is greater than or equal to 3. ")  
+            \n''')
+    
+        proceed()
+
+        print('The above structure will print all True conditions and not print any False Conditions. Try it.\n')
+    
+        # if if if example.
+        quantity = int(input("Select a number between 1-3: \n"))
         if quantity >= 1:
-            print ("Quantity is greater than or equal to 1. ")
+            print ("Quantity is greater than or equal to 1.\n")
         if quantity >= 2:
-            print ("Quantity is greater than or equal to 2. ")
+            print ("Quantity is greater than or equal to 2.\n")
         if quantity >= 3:
-            print ("Quantity is greater than or equal to 3. ")  
+                print ("Quantity is greater than or equal to 3.\n")
+
+        print('''
+            If 1 is selected only "Quantity is greater than or equal to 1." will return.\n
+            if 2 is selected "Quantity is greater than or equal to 1.", and "Quantity is greater than or equal to 2." will return\n
+            if 3 is selected all three statements will return.
+            ''')
+    
+
+    def ifElifElifElseSelections():
+   
+        print ('''
+            IF ELIF ELIF ELSE SELECTIONS\n
+            In an IF ELIF ELSE Selection...
+            Python will search the entire structure for tho only true condition test return the true value.
+            In these selections as many ELIF statements as needed can be written as needed.
+            If the value is not found in any ELIF structures, it will execute the else branch.\n
+            ''')
+        
+        print('Proceed to try the Mass Effect example.\n')
+    
+
+        # if elif else ... example.
+        legion_says = int(input("Enter a number between 1-3: "))
+        if legion_says <= 1:
+            print ("Heretics say 1 is less than 2.\n")
+        elif legion_says <=2:
+            print ("Heretics say 2 is greater than 1.\n")
+        elif legion_says == 3:
+            print ("Geth say 3 is greater than 1 and 2.\n")
+        else:
+            print("You do not follow directions Shepard Commander, you must be indoctrinated.\n")
+
+        print ('''
+            In this example, if you picked 1 the program would have returned\n
+            Heretics say 1 is less than 2\n
+           
+            If you picked 2\n
+   
+            Heretics say 2 is greater than 1\n
+           
+            If 3 was chosen, it would have printed Legion's line from Mass Effect 2\n
+           
+            If anything other than 1,2, or 3 was chosen the program would have admonished you for not following directions,
+            by executing the else branch.\n
+            ''')
+    
+        proceed()
+
+        print ('''
+            IF ELIF ELSE SELECTIONS EXAMPLE 2\n
+            IF ELIF ELSE SELECTIONS are a structure that will ONLY EXECUTE THE FIRST CONDITION THAT IS TRUE.\n
+            If greater than 3 conditions are needed to complete this sort of structure, ONLY ADDITIONAL ELIF Conditions are created.
+            Next we'll explore a IF ELIF ELSE SELECTION using the Sorting Hat from Harry Potter.\n
+            ''')
+        
+        print('Proceed to see how the Sorting Hat might judge you, no pressure.')
+
+        proceed()
+    
+        # if elif else example
+        sorting_hat_selection = (input("What virtue do you value most? bravery, intelligence, or friendship: \n"))
+        if sorting_hat_selection == ("bravery"):
+            print ("You are in House Gryffindor\n")
+        elif sorting_hat_selection == ("intelligence"):
+            print ("You are in House Ravenclaw.\n")
+        elif sorting_hat_selection == "friendship":
+            print ("You are in House Hufflepuff.\n")
+        else: print ("You are in House Slytherin.\n")
+
+        proceed()
+
+        print ('''
+            Since this is an IF ELIF... ELSE structure it will only return the true statement.
+            IF "bravery was selected, the user is Gryffindor; intelligence Ravenclaw
+            friendship Hufflepuff.And if you don't follow rules, the else statement captures
+            all other values, Slytherin.\n
+            ''')
+    
+        proceedToMenu()
+    
+    while True:
+        
+        # declarations for inputs() menu
+        selections_menu =('''
+        MENU - Selections
+        ........................................................................
+        1: Selection Basics
+        2: If Else Selections
+        3: If, If, If... Selections
+        4: If ELif Else Selections            
+        OR ANY OTHER KEY TO EXIT.
+        .........................................................................
         \n''')
-    
-    proceed()
+        
+        print(selections_menu)
+        selection_selection = int(input('Select a Topic \n'))
 
-    print('The above structure will print all True conditions and not print any False Conditions. Try it.\n')
-    
-    # if if if example.
-    quantity = int(input("Select a number between 1-3: \n"))
-    if quantity >= 1:
-        print ("Quantity is greater than or equal to 1.\n")
-    if quantity >= 2:
-        print ("Quantity is greater than or equal to 2.\n")
-    if quantity >= 3:
-        print ("Quantity is greater than or equal to 3.\n")
+        if selection_selection == 1:
+            print(atopic)
+            selectionBasics()
+        elif selection_selection == 2:
+            print(atopic)
+            ifElseSelections()
+        elif selection_selection == 3:
+            print(atopic)
+            ifIfIfSelections()
+        elif selection_selection == 4:
+            print(atopic)
+            ifElifElifElseSelections()
+        else:
+            print(ex)
+            break
 
-    print('''
-        If 1 is selected only "Quantity is greater than or equal to 1." will return.\n
-        if 2 is selected "Quantity is greater than or equal to 1.", and "Quantity is greater than or equal to 2." will return\n
-        if 3 is selected all three statements will return.
-        ''')
-    
-
-    proceed()
-   
-    print ('''
-        IF ELIF ELIF ELSE SELECTIONS\n
-        In an IF ELIF ELSE Selection...
-        Python will search the entire structure for tho only true condition test return the true value.
-        In these selections as many ELIF statements as needed can be written.
-        If the value is not found, it will execute the else branch.\n
-        ''')
-    
-
-    # if elif else ... example.
-    legion_says = int(input("Enter a number between 1-3: "))
-    if legion_says <= 1:
-        print ("Heretics say 1 is less than 2.\n")
-    elif legion_says <=2:
-        print ("Heretics say 2 is greater than 1.\n")
-    elif legion_says == 3:
-        print ("Geth say 3 is greater than 1 and 2.\n")
-    else:
-        print("You do not follow directions Shepard Commander, you must be indoctrinated.\n")
-
-    print ('''
-        In this example, if you picked 1 the program would have returned\n
-        Heretics say 1 is less than 2\n
-           
-        If you picked 2\n
-   
-        Heretics say 2 is greater than 1\n
-           
-        If 3 was chosen, it would have printed Legion's line from Mass Effect 2\n
-           
-        If anything other than 1,2, or 3 was chosen the program would have admonished you for not following directions,
-        by executing the else branch.\n
-        ''')
-    
-    proceed()
-
-    print ('''
-        IF ELIF ELSE SELECTIONS\n
-        IF ELIF ELSE SELECTIONS are a structure that will ONLY EXECUTE THE FIRST CONDITION THAT IS TRUE.\n
-        If greater than 3 conditions are needed to complete this sort of structure, ONLY ADDITIONAL ELIF Conditions are created.
-        Next we'll explore a IF ELIF ELSE SELECTION using the Sorting Hat from Harry Potter.\n
-        ''')
-    
-    # if elif else example
-    sorting_hat_selection = (input("What virtue do you value most? bravery, intelligence, or friendship: \n"))
-    if sorting_hat_selection == ("bravery"):
-        print ("You are in House Gryffindor\n")
-    elif sorting_hat_selection == ("intelligence"):
-        print ("You are in House Ravenclaw.\n")
-    elif sorting_hat_selection == "friendship":
-        print ("You are in House Hufflepuff.\n")
-    else: print ("You are in House Slytherin.\n")
-
-    proceed()
-
-    print ('''
-        Since this is an IF ELIF... ELSE structure it will only return the true statement.
-        IF "bravery was selected, the user is Gryffindor; intelligence Ravenclaw
-        friendship Hufflepuff.And if you don't follow rules, the else statement captures
-        all other values, Slytherin.\n
-        ''')
-    
-    proceedToMenu()
-    
 def inputValidation(): # input validation function.
 
     print ('''
