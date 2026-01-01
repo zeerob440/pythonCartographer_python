83aaadb23df75af156cc891d2ca3a0b194fdad03
zeerob440 <zeerob440@tutanota.com>
2026-01-01 14:01:13 -0500

    fixed: lines 11- 18 fixed proceedToMenu and proceed behavior for real, for real now.

--- pythonCartographer.py
+++ pythonCartographer.py
@@ -10,11 +10,11 @@ print ("This program helps navigate notes and simple structures in the Python la
 
 # . Prints valid input selection, runs through indefinite loop.
 def proceed(): # It asks the user to continue after each example within a module
-    any_key_to_continue = print('''\n
+    any_key_to_continue = input('''\n
                         Press any key to proceed\n''')
 
 def proceedToMenu():
-    any_key_to_menu = print('''\n
+    any_key_to_menu = input('''\n
                     This concludes this module. Press any key to continue.\n''')
     
 
