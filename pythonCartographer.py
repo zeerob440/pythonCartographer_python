b5c3cae60e764a016d34a1123b4e445bd6d9ffa2
zeerob440 <zeerob440@tutanota.com>
2026-01-02 16:57:51 -0500

    fixed: added depopulatingLists() and daughter functions depopulateListsBasics(), depopulateWList(), and depopulateWListComp9)

--- pythonCartographer.py
+++ pythonCartographer.py
@@ -1149,7 +1149,7 @@ def listsPopulation():
                 if sentinel_prompt == ('y'):      
                     append_list = True
                 else: append_list = False
-        print(*another_list)# * is used for argument unpacking it removes commas and brackets.
+        print(*another_list)# * IS USED FOR UNPACKING LISTS IT REMOVES ALL THE COMMAS AND BRACKETS.
         Below this code will prompt you to populate a list until you input 'n'\n
         ''')
 
@@ -1261,12 +1261,16 @@ def listsPopulation():
 
 def depopulatingLists():
 
-    print('''DEPOPULATING LISTS\n
+    def depopulateListBasics():
+
+        print('''DEPOPULATING LISTS\n
           Modifying lists is a typical workflow in Python, items need to be added, and items need to be removed.
           We will explore how to depopulate lists with the pop() and remove() methods, and typical structures whereby these methods are
           used.\n''')
+        
+        proceed()
     
-    print('''DEPOPULATING LIST WITH pop() and remove()\n
+        print('''DEPOPULATING LIST WITH pop() and remove()\n
           The pop() method is a technique to remove the LAST ITEM of a list if no argument is added, or it can remove the item
           in a specific index if an argument is added, below is an example of how pop() is used in both manners.
           remove() removes the first occurrence of the value specified in its arg.\n
@@ -1281,54 +1285,85 @@ def depopulatingLists():
           
         Proceed to run the code.\n''')
     
-    proceed()
+        proceed()
     
-    the_list = ['dog', 'cat', 'bird', 'cat', 'whale', 'cat', 'dolphin']
-    the_list.pop() #removes last item of list "dolphin
-    print(the_list)
-    the_list.pop(2) #removes item in the second index "bird"
-    print(the_list)
-    the_list.remove('cat') #removes the first "cat" from list
-    print(the_list)
+        the_list = ['dog', 'cat', 'bird', 'cat', 'whale', 'cat', 'dolphin']
+        the_list.pop() #removes last item of list "dolphin
+        print(the_list)
+        the_list.pop(2) #removes item in the second index "bird"
+        print(the_list)
+        the_list.remove('cat') #removes the first "cat" from list
+        print(the_list)
 
-    proceed()
+        proceedToMenu()
 
-    print('''REMOVING LIST ITEMS WITH ANOTHER LIST\n
+    def depopulateListWList():
+        print('''REMOVING LIST ITEMS WITH ANOTHER LIST\n
         Items can be removed from one list by another list with remove(). This is done by iterating through one list
         and then searching for duplicate items in another list and removing them.
         like this:\n
         
 
-    list26 = [1, 2, 3, 4, 5, 6]
-    list62 = [2, 4, 6]
+        list26 = [1, 2, 3, 4, 5, 6]
+        list62 = [2, 4, 6]
 
-    for item26 in list26:
-        if item26 in list62:
-            list26.remove(item26)
-    print(*list26)\n
+        for item26 in list26:
+            if item26 in list62:
+                list26.remove(item26)
+        print(*list26)\n
           
-    It will output 1 3 5 since * unpacker is used here
+        It will output 1 3 5 since * unpacker is used here
     
-    Proceed to run the code''')
+        Proceed to run the code''')
 
-    proceed()
+        proceed()
 
-    # remove items from one list that are in another.
+        # remove items from one list that are in another.
 
-    list26 = [1, 2, 3, 4, 5, 6]
-    list62 = [2, 4, 6]
+        list26 = [1, 2, 3, 4, 5, 6]
+        list62 = [2, 4, 6]
 
-    for item26 in list26:
-        if item26 in list62:
-            list26.remove(item26)
-    print(*list26)
+        for item26 in list26:
+            if item26 in list62:
+                list26.remove(item26)
+        print(*list26)
 
-    proceed()
+        proceedToMenu()
 
-    print('''REMOVE WITH LIST COMPREHENSION\n
-        FIXME: create list comprehension example. ''')
+    def depopulateWListComp():
 
-    proceedToMenu()
+        print('''REMOVE WITH LIST COMPREHENSION\n
+            FIXME: create list comprehension example. ''')
+
+        proceedToMenu()
+
+    while True:
+        
+        # declarations for depopulateLists() menu
+        depopulate_menu =('''
+        MENU - Depopulating Lists
+        ........................................................................
+        1: Depopulate List  Basics
+        2: Flag Controlled While Loops            
+        OR ANY OTHER NUMBER TO EXIT.
+        .........................................................................
+        \n''')
+        
+        print(depopulate_menu)
+        depopulate_selection = int(input('Select a Topic \n'))
+
+        if depopulate_selection == 1:
+            print(atopic)
+            depopulateListBasics()
+        elif depopulate_selection == 2:
+            print(atopic)
+            depopulateListWList()
+        elif depopulate_selection == 3:
+            print(atopic)
+            depopulateWListComp
+        else:
+            print(ex)
+            break    
         
      
 def main():# it serves as the main menu of pythonCartographer, one may call it the "index".
