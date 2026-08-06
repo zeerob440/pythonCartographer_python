be7829a1d5b7d5531498a62e042fde7eea3e2722
MU-TH-ER <zeerob440@tutanota.com>
2026-08-06 15:08:12 -0400

    debug: __main__.py| importing basics.py still unsucessful

---
+++ navigation.py
@@ -0,0 +1,25 @@
+'''
+navigation.py contains class methods that enable the user to navigate the application. 
+'''
+
+class PROCEED_CLASS():
+    def proceed(): # It asks the user to continue after each example within a lesson.
+        any_key_to_continue = input('''\n
+            Press any key to proceed.\n''')
+
+class PROCEED_TO_MENU_CLASS():
+    def proceedToMenu():
+        any_key_to_menu = input('''\n
+                This concludes this module. Press any key to continue.\n''')
+    
+# Used to denote that content is being accessed, informs user when exiting.
+class ATOPIC_Y_EXIT():
+    
+    def atopic():
+        statement = 'Accessing Topic...\n'
+        print(statement)
+
+    #ex used to declare that a menu is exiting. 
+    def exiting():
+        ex = 'Exiting...\n'
+        print(ex)
