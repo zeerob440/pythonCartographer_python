0abfea1ccf03be76cfb4d928fd342fe039e3b96d
MU-TH-ER <zeerob440@tutanota.com>
2026-08-19 16:08:41 -0400

    update: for_loops.pt| updated formatting.

--- for_loops.py
+++ for_loops.py
@@ -6,38 +6,40 @@ class FOR_LOOPS_MENU():
 
     @staticmethod
     def forLoopBasics():
-        print('''
-        FOR LOOPS\n
-        FOR LOOPS are a type of indefinite loop. They are used to do various tasks such as populating LISTS,
-        and completing a task in a set amount of iterations. Iterations are the number of 'laps' ran around the loop.
-        FOR LOOPS require a PRIMING VALUE,and a Counter(INCREMENTOR OR DECREMENTOR)
-        FOR LOOPS are often described as "for i in range of.." in classroom settings.\n
+        print(
+'''
+FOR LOOPS\n
+FOR LOOPS are a type of indefinite loop. They are used to do various tasks such as populating LISTS,
+and completing a task in a set amount of iterations. Iterations are the number of 'laps' ran around the loop.
+FOR LOOPS require a PRIMING VALUE,and a Counter(INCREMENTOR OR DECREMENTOR)
+FOR LOOPS are often described as "for i in range of.." in classroom settings.\n
               
-        To set the range on a for loop set as:\n
+To set the range on a for loop set as:\n
               
-        for i in range(number goes here)
-        ''')
+    for i in range(number goes here)
+''')
 
         PROCEED_CLASS.proceed()
 
-        print('''LOOP VARIABLES\n
-            loop variables are unusual compared to other var declarations. In the following example:\n
+        print(
+'''LOOP VARIABLES\n
+loop variables are unusual compared to other var declarations. In the following example:\n
               
-            for i in range(3):
-                print(i)
-            print(i)\n
+    for i in range(3):
+    print(i)
+    print(i)\n
             
-            i is the loop variable, it is declared to iterate through the loop, it can just as easily be named
-            "widgets" or "doodads". However, loop variables are intended to be used within the scope of the
-            for loop. If it is printed outside the scope of the loop, it outputs its last value from when it was in the loop.\n
+i is the loop variable, it is declared to iterate through the loop, it can just as easily be named
+"widgets" or "doodads". However, loop variables are intended to be used within the scope of the
+for loop. If it is printed outside the scope of the loop, it outputs its last value from when it was in the loop.\n
             
-            The example will output\n
-            0
-            1
-            2 # last iteration of for loop
-            2 # print statement outside of the loop's scope\n
-            Proceed to run the program.
-             ''')
+The example will output\n
+    0
+    1
+    2 # last iteration of for loop
+    2 # print statement outside of the loop's scope\n
+Proceed to run the program.
+''')
         
         PROCEED_CLASS.proceed()
         for i in range(3):
@@ -50,22 +52,23 @@ class FOR_LOOPS_MENU():
     def rangeControl():
     #Create simple "for i in range of" FOR LOOP
     
-        print('''FOR LOOP RANGE CONTROL\n
-            In this example you will see...\n
-            0
-            1
-            2\n
-            That is because all iterations start with 0 by default in Python. Unless specified otherwise\n
-            Proceed to run the code.''')
+        print(
+'''FOR LOOP RANGE CONTROL\n
+In this example you will see...\n
+    0
+    1
+    2\n
+That is because all iterations start with 0 by default in Python. Unless specified otherwise\n
+Proceed to run the code.''')
         
         PROCEED_CLASS.proceed()
 
-        print('''The following is a hardcoded simple FOR LOOP that prints number of iterations.\n
+        print(
+'''The following is a hardcoded simple FOR LOOP that prints number of iterations.\n
         
-        for i in range(3):
-            print(i)
-        
-        \n''')
+    for i in range(3):
+        print(i)\n
+''')
 
         for i in range(3):
             print(i)
@@ -73,46 +76,50 @@ class FOR_LOOPS_MENU():
 
         PROCEED_CLASS.proceed()
     
-        print('''
-            RANGE CONTROL EXAMPLE/n
-            To control range in a for loop, it can be declared as\n
-            for i in range (1, 4):
-                print(i)\n
-            print("The RANGE CONTROL the above example starts at 1, and ALWAYS END AT THE HIGHEST DECLARED NUMBER - 1.\n")
-            Proceed to run this example!''')
+        print(
+'''
+RANGE CONTROL EXAMPLE\n
+To control range in a for loop, it can be declared as\n
+    for i in range (1, 4):
+    print(i)\n
+
+The RANGE CONTROL the above example starts at 1, and ALWAYS END AT THE HIGHEST DECLARED NUMBER - 1.\n")
+Proceed to run this example!''')
         
         PROCEED_CLASS.proceed()
 
         for i in range (1,4):
             print(i)
-        print("The RANGE CONTROL the above example starts at 1, and ALWAYS END AT THE HIGHEST DECLARED NUMBER - 1.\n")
+        print('''
+The RANGE CONTROL the above example starts at 1, and ALWAYS END AT THE HIGHEST DECLARED NUMBER - 1.\n
+''')
 
         PROCEED_TO_MENU_CLASS.proceedToMenu()
 
     @staticmethod
     def eLoopControl():
 
-        print('''Sometimes loop readability is in itself valuable. Especially if work with
-            elements in the iterable is needed later in the program.\n
-            When such usecases present it good to name the loop var and iterate through the list:
-             hockey_league: list = ['Panthers', 'Blackhawks', 'Habs']
+        print(
+'''Sometimes loop readability is in itself valuable. Especially if work with
+elements in the iterable is needed later in the program.\n
+When such usecases present it good to name the loop var and iterate through the list:
+    hockey_league: list = ['Panthers', 'Blackhawks', 'Habs']
             
-                    for team in hockey_league:
-                        print(team)
+        for team in hockey_league:
+            print(team)
             
-                    # later in program find team in the iterable  
-                    print(hockey_league[2])
-                    print(hockey_league.index('Panthers'))
+     # later in program find team in the iterable  
+    print(hockey_league[2])
+    print(hockey_league.index('Panthers'))
             
-                    # search for a team in the list with 'in' key word
-                    if 'Panthers' in hockey_league:
-                        print('Vamos Gatos!')
-                    else:
-                        print('No Gatos here.')
+    # search for a team in the list with 'in' key word
+        if 'Panthers' in hockey_league:
+            print('Vamos Gatos!')
+        else:
+            print('No Gatos here.')
 
-            below you will run this example. 
-
-            \n''')
+below you will run this example.\n
+''')
 
         PROCEED_CLASS.proceed()
 
